36ed7b4972c0912ca7f079cebbadb9ee156b32c3
Eliezer Wohl <eliezerwohl@gmail.com>
2016-11-16 13:25:23 -0500

    radio now make some pics invisible or visibile, depending on what selected

--- app/src/main/java/com/example/eliezerwohl/kingofdice/MainActivity.java
+++ app/src/main/java/com/example/eliezerwohl/kingofdice/MainActivity.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import static android.R.attr.button;
 import static android.R.attr.checked;
+import static com.example.eliezerwohl.kingofdice.R.id.image7;
 import static com.example.eliezerwohl.kingofdice.R.id.radio6;
 import static com.example.eliezerwohl.kingofdice.R.id.radioGroup;
 
@@ -35,25 +37,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageButton image1 = (ImageButton) findViewById(R.id.image1);
+        ImageButton image2 = (ImageButton) findViewById(R.id.image2);
+        ImageButton image3 = (ImageButton) findViewById(R.id.image3);
+        ImageButton image4 = (ImageButton) findViewById(R.id.image4);
+        ImageButton image5 = (ImageButton) findViewById(R.id.image5);
+        ImageButton image6 = (ImageButton) findViewById(R.id.image6);
+       final ImageButton image7 = (ImageButton) findViewById(R.id.image7);
+       final ImageButton image8 = (ImageButton) findViewById(R.id.image8);
         RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int view) {
                 switch (view) {
                     case R.id.radio6:
-                        Log.d("SWITCH", "GOT 6");
+                        image7.setVisibility(View.INVISIBLE);
+                        image8.setVisibility(View.INVISIBLE);
                         break;
                     case R.id.radio7:
                         Log.d("SWITCH", "GOT 7");
+                        image7.setVisibility(View.VISIBLE);
+                        image8.setVisibility(View.INVISIBLE);
                         break;
                     case R.id.radio8:
-                        Log.d("SWITCH", "GOT 8");
+                        image7.setVisibility(View.VISIBLE);
+                        image8.setVisibility(View.VISIBLE);;
                         break;
                 }
 
             }
         });
-
 
     }
 }
