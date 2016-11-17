c3e608c91da9204e61b8aaddaba71634f15925a6
Eliezer Wohl <eliezerwohl@gmail.com>
2016-11-16 19:31:56 -0500

    cleaned up tthe interface

--- app/src/main/java/com/example/eliezerwohl/kingofdice/Click.java
+++ app/src/main/java/com/example/eliezerwohl/kingofdice/Click.java
@@ -1,8 +1,22 @@
 package com.example.eliezerwohl.kingofdice;
+import android.widget.ImageButton;
+
+import java.util.Random;
 
 /**
  * Created by Elie on 11/16/2016.
  */
 
 public class Click {
+    public void roll(int[] images, ImageButton[] buttonIDs){
+        for(int i=0; i<buttonIDs.length; i++) {
+            ImageButton temp = buttonIDs[i];
+
+            if (temp.getAlpha() != 0.5f) {
+                Random rand = new Random();
+                int randomIndex = rand.nextInt(images.length);
+                temp.setImageResource(images[randomIndex]);
+            }
+        }
+    }
 }
